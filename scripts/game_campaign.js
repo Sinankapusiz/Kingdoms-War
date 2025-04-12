63a7e94d8bccc8dc37f607cdd8524badec659726
Sinan Kapusız <sinankapusiz@outlook.com>
2025-04-12 22:19:22 +0300

    şehir barakları ve bölge renkleri sahiplerine göre atayan fonksiyon ve değerleri yazıldı.

--- scripts/game_campaign.js
+++ scripts/game_campaign.js
@@ -3,17 +3,43 @@
 const sehirler = [];
 const sehirlerLabel = document.querySelectorAll(".cityNameLabel");
 
-function sehirOlustur(pathJson){
+
+
+function sehirOlustur(pathJson) {
     fetch(pathJson)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach((sehir,index) => {
-            console.log(`Şehir ${index+1}:${sehir.sehir_adi} Oluşturuldu.`);
-            sehirlerLabel[index].textContent = sehir.sehir_adi;
-            sehirler.push(sehir);
-        });
-    })
-    .catch(error => console.error('Şehir oluşturulken hata oluştu:', error)
-);
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((sehir, index) => {
+                console.log(`Şehir ${index + 1}:${sehir.sehir_adi} Oluşturuldu.`);
+                sehirlerLabel[index].textContent = sehir.sehir_adi;
+                sehirler.push(sehir);
+            });
+        })
+        .catch(error => console.error('Şehir oluşturulken hata oluştu:', error)
+        );
 };
 sehirOlustur('./scripts/data/cities_data.json');
+
+function sehirSahiplerininBayraginiAta(pathJson) {
+    fetch(pathJson)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((ulke, index) => {
+                ulke.ulke_sahip_oldugu_sehirler.forEach((city) => {
+                    const cityN = document.getElementById(city[0]);
+                    const cityFlagSembol = cityN.querySelector("path");
+                    const cityFlagBackground = cityN.querySelector("polygon");
+
+                    cityFlagSembol.setAttribute('d', ulke.ulke_bayragi.ulke_bayragi_sembol);
+                    cityFlagSembol.id = ulke.ulke_bayragi.ulke_bayragi_sembol_color;
+                    cityFlagBackground.id = ulke.ulke_bayragi.ulke_bayragi_arkaplan;
+                    
+                    const cityRegion = document.getElementById(city[2]);
+                    cityRegion.style.fill = ulke.ulke_bolge.bolge_rengi;
+                    cityRegion.style.stroke = ulke.ulke_bolge.bolge_kenar_rengi;
+                    cityRegion.style.strokeWidth = "10px";
+                })
+            })
+        })
+};
+sehirSahiplerininBayraginiAta('./scripts/data/countries_data.json');
